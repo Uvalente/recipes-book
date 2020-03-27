e93c7d8830b8939735c81e2671ff9b2b043f2804
Umberto Valente <uvalente89@gmail.com>
2020-03-27 14:18:27 +0000

    feat: add basic footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer>I am the footer</footer>
+    <footer>
+      I am the footer.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis suscipit nisl sit amet tristique. Curabitur dapibus nisl et sodales aliquet. Cras vulputate ipsum interdum diam bibendum, vitae euismod nibh blandit. Morbi id nunc commodo, viverra risus quis, facilisis ante. Ut dapibus lorem vitae facilisis vehicula.
+    </footer>
   )
 }
 
