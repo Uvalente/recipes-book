4138eb28acd550beb55592bb7b420840161ef467
Umberto Valente <uvalente89@gmail.com>
2020-03-27 14:13:30 +0000

    feat: add basic header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
     <nav>
       <h1>Your recipe book</h1>
+      <div>
+        Menuplaceholder
+      </div>
     </nav>
   )
 }
