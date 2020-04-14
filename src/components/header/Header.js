ae3d0d51679305b7fda9d979d52735fa9225e0f9
Umberto Valente <uvalente89@gmail.com>
2020-04-14 13:04:38 +0100

    feat: add signout button to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../../firebase'
 import './Header.css'
 
 const Header = () => {
+  const doSignOut = ()=> auth.signOut()
+  const SignoutButton = () => <button onClick={doSignOut}>Sign Out</button>
+
   return (
-      <nav>
-        <h1>Your recipe book</h1>
-        <div>
-          <Link to={'/'}>Home</Link>
-          <Link to={'/recipes/new'}>Add Recipe</Link>
-          <Link to={'/signup'}>Register</Link>
-          <Link to={'/login'}>Log In</Link>
-        </div>
-      </nav>
+    <nav>
+      <h1>Your recipe book</h1>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/recipes/new'}>Add Recipe</Link>
+        <Link to={'/signup'}>Register</Link>
+        <Link to={'/login'}>Log In</Link>
+        <SignoutButton />
+      </div>
+    </nav>
   )
 }
 
