926501670a9cc74796f69c8402418a9ba2412020
Umberto Valente <uvalente89@gmail.com>
2020-06-01 16:44:13 +0100

    feat: add loading indicator while loading recipes

--- src/components/recipeCollection/RecipeCollection.js
+++ src/components/recipeCollection/RecipeCollection.js
@@ -2,21 +2,30 @@ import React, { useState, useEffect } from 'react'
 import RecipeCard from '../recipeCard/RecipeCard'
 import { db } from '../../firebase'
 
-
 const RecipeCollection = (props) => {
   const [recipeList, setRecipeList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   console.log('I am loading 4 times?! RecipeCollection')
-  useEffect(() => {
-    db.collection(`users/${props.user.uid}/recipes`)
-      .orderBy('name')
-      .onSnapshot(snapShot => {
-        const recipes = []
-        snapShot.forEach(recipe => {
-          recipes.push({id: recipe.id, data: recipe.data()})
-        })
-        setRecipeList(recipes)
-      })
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      let recipeRef = db.collection(`users/${props.user.uid}/recipes`)
+      await recipeRef.orderBy('name').get()
+        .then(snapshot => {
+          const recipes = []
+          snapshot.forEach(recipe => {
+            recipes.push({ id: recipe.id, data: recipe.data() })
+          })
+          setRecipeList(recipes)
+          setIsLoading(false)
+        })
+        .catch(err => {
+          console.log('Error getting recipes', err);
+        })
+    }
+
+    fetchData()
   }, [])
 
   const recipeComponents = recipeList.map(recipe =>
@@ -29,9 +38,8 @@ const RecipeCollection = (props) => {
     />
   )
 
-  return (
-    recipeComponents
-  )
+  return isLoading ? (< h1 > Loading...</h1 >) : recipeComponents
+
 }
 
 export default RecipeCollection
