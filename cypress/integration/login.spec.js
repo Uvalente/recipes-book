3130b21c2f410512b78cbfa9dfc09059848a92a1
Umberto Valente <uvalente89@gmail.com>
2020-06-02 03:28:56 +0100

    chore: install cypress, add integration test

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,16 @@
+describe('Recipe creation', () => {
+  it('user can log in and create a recipe', () => {
+    cy.visit('/login')
+    cy.get('input[name=email]').type('test@example.com')
+    cy.get('input[name=password').type('password{enter}')
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.contains('Test recipe book').should('exist')
+    cy.contains('Add Recipe').click()
+    cy.get('textarea').type('Instructions')
+    cy.get('select').select('Dessert')
+    cy.get('input[name=recipeName]').type('Recipe Name{enter}')
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.contains('Instructions').should('exist')
+    cy.contains('Read more...').should('exist')
+  })
+})
