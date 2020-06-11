adb9b056a94cf1cb90a6c58b82a4fec8d815754d
Umberto Valente <uvalente89@gmail.com>
2020-06-11 17:17:42 +0100

    feat: restyle Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='bg-blue-900 text-gray-400 px-6 py-6 lg:py-10 lg:px-12 text-xs'>
       I am the footer.
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis suscipit nisl sit amet tristique. Curabitur dapibus nisl et sodales aliquet. Cras vulputate ipsum interdum diam bibendum, vitae euismod nibh blandit. Morbi id nunc commodo, viverra risus quis, facilisis ante. Ut dapibus lorem vitae facilisis vehicula.
     </footer>
