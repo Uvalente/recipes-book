443641301d3763aa6ddcf3c268999db6d7c8e7c1
Umberto Valente <uvalente89@gmail.com>
2020-06-11 12:37:45 +0100

    feat: restyle RecipeCard

--- src/components/recipeCard/RecipeCard.js
+++ src/components/recipeCard/RecipeCard.js
@@ -1,39 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './RecipeCard.css'
 
 const RecipeCard = (props) => {
   return (
-    <div className='recipe-card'>
-      <Link to={`/recipes/${props.id}`} >
-        <div className='top-card'>
+    <div className='w-1/2 sm:w-1/3 lg:w-1/4 xl:w-1/5 p-2 lg:p-4 xl:p-6'>
+      <div className='relative pb-5/6'>
+        <Link to={`/recipes/${props.id}`} >
           <img
+            className='absolute h-full w-full object-cover rounded-lg shadow-md'
             src={props.recipeImageUrl || "/no_image.jpg"}
             alt={props.recipeName}
           />
-        </div>
-        <div className='bottom-card'>
-          <p data-testid='test-title'>
-            {
-              props.recipeName.length > 30 ?
-                props.recipeName.slice(0, 30) + '...'
-                :
-                props.recipeName
-            }
-          </p>
-          <p className='course' data-testid='test-course'>
-            {props.recipeCourse}
-          </p>
-          {/* <p className="recipe-description" data-testid='test-description'>
-          {
-            props.recipeDescription.length > 400 ?
-              props.recipeDescription.slice(0, 400) + '...'
-              :
-              props.recipeDescription
-          }
-        </p> */}
-        </div>
-      </Link>
+        </Link>
+      </div>
+      <div className='relative px-4 -mr-8 -mt-8'>
+        <Link to={`/recipes/${props.id}`} >
+          <div className='bg-white py-2 px-4 rounded-lg shadow-lg'>
+            <p
+              className='text-gray-600 text-xs uppercase font-semibold tracking-wide'
+            >
+              {props.recipeCourse}
+            </p>
+            <p
+              className='text-gray-900 text-lg font-semibold truncate'
+            >
+              {props.recipeName}
+            </p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
