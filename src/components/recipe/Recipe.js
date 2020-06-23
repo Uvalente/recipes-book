7d2ca34df30fc1d091e258d40ef0e2992fc8e724
Umberto Valente <uvalente89@gmail.com>
2020-06-23 03:19:26 +0100

    feat: style delete button

--- src/components/recipe/Recipe.js
+++ src/components/recipe/Recipe.js
@@ -72,18 +72,21 @@ const Recipe = () => {
             {`${description}`}
           </div>
 
-          <div className='space-x-2'>
-            <Link to={`/users/${uid}`}>
-              <button className='mt-6 bg-blue-700 hover:bg-blue-600 text-white font-bold py-1 px-4 border-b-4 border-blue-900 hover:border-blue-800 rounded-md md:mt-8 lg:mt-10'>Back</button>
-            </Link>
-            {
-              auth.currentUser && auth.currentUser.uid === uid &&
-              <Fragment>
+          <div className='flex justify-between'>
+            <div className='space-x-2'>
+              <Link to={`/users/${uid}`}>
+                <button className='mt-6 bg-blue-700 hover:bg-blue-600 text-white font-bold py-1 px-4 border-b-4 border-blue-900 hover:border-blue-800 rounded-md md:mt-8 lg:mt-10'>Back</button>
+              </Link>
+              {
+                auth.currentUser && auth.currentUser.uid === uid &&
                 <Link to={`/users/${uid}/recipes/${id}/edit/`}>
                   <button className='mt-6 bg-blue-700 hover:bg-blue-600 text-white font-bold py-1 px-4 border-b-4 border-blue-900 hover:border-blue-800 rounded-md md:mt-8 lg:mt-10'>Edit</button>
                 </Link>
-                <button className='mt-6 bg-blue-700 hover:bg-blue-600 text-white font-bold py-1 px-4 border-b-4 border-blue-900 hover:border-blue-800 rounded-md md:mt-8 lg:mt-10' onClick={handleDelete}>Delete</button>
-              </Fragment>
+              }
+            </div>
+            {
+              auth.currentUser && auth.currentUser.uid === uid &&
+              <button className='mt-6 bg-red-700 hover:bg-red-600 text-white font-bold py-1 px-4 border-b-4 border-red-900 hover:border-red-800 rounded-md md:mt-8 lg:mt-10' onClick={handleDelete}>Delete</button>
             }
           </div>
         </div>
